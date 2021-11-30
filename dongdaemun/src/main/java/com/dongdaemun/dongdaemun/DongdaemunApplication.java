69e50e41e3b137c398e5cc0d31cf7d9e7906c8ff
cutehumanS2 <60508828+cutehumanS2@users.noreply.github.com>
2021-11-30 11:53:59 +0900

    Init React & Spring boot

---
+++ dongdaemun/src/main/java/com/dongdaemun/dongdaemun/DongdaemunApplication.java
@@ -0,0 +1,13 @@
+package com.dongdaemun.dongdaemun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DongdaemunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DongdaemunApplication.class, args);
+	}
+
+}
---
+++ dongdaemun/src/test/java/com/dongdaemun/dongdaemun/DongdaemunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongdaemun.dongdaemun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DongdaemunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
