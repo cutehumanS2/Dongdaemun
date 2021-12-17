25f0dfda80ced8782e8aa8ab421f9728121ee4cf
cutehumanS2 <60508828+cutehumanS2@users.noreply.github.com>
2021-12-17 21:11:42 +0900

    게시글 수정 & 삭제 & 테스트코드 구현

--- dongdaemun/src/main/java/com/dongdaemun/dongdaemun/web/PostsApiController.java
+++ dongdaemun/src/main/java/com/dongdaemun/dongdaemun/web/PostsApiController.java
@@ -5,20 +5,33 @@ import com.dongdaemun.dongdaemun.config.auth.dto.SessionUser;
 import com.dongdaemun.dongdaemun.service.PostsService;
 import com.dongdaemun.dongdaemun.web.dto.PostsListResponseDto;
 import com.dongdaemun.dongdaemun.web.dto.PostsResponseDto;
+import com.dongdaemun.dongdaemun.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class PostsApiController {
     private final PostsService postsService;
 
+    /*게시글 수정*/
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    /*게시글 조회*/
     @GetMapping("/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
 
         return postsService.findById(id);
+    }
+
+    /*게시글 삭제*/
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
     }
 
 }
