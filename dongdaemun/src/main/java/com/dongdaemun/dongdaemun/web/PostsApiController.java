d72af29413bab0a3c4e4d64a7cc386b55e4ef68b
Mingadinga <minpearl0826@gmail.com>
2021-12-18 20:06:38 +0900

    페이징 처리 컨트롤러 수정 : ModelAndView 반환

--- dongdaemun/src/main/java/com/dongdaemun/dongdaemun/web/PostsApiController.java
+++ dongdaemun/src/main/java/com/dongdaemun/dongdaemun/web/PostsApiController.java
@@ -1,18 +1,14 @@
 package com.dongdaemun.dongdaemun.web;
 
-import com.dongdaemun.dongdaemun.config.auth.LoginUser;
-import com.dongdaemun.dongdaemun.config.auth.dto.SessionUser;
 import com.dongdaemun.dongdaemun.domain.posts.Posts;
 import com.dongdaemun.dongdaemun.service.PostsService;
-import com.dongdaemun.dongdaemun.web.dto.PostsListResponseDto;
 import com.dongdaemun.dongdaemun.web.dto.PostsResponseDto;
 import com.dongdaemun.dongdaemun.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequiredArgsConstructor
 @RestController
@@ -32,6 +28,7 @@ public class PostsApiController {
         else return null;
     }
 
+    /*
     @GetMapping("/board/{category}")
     public String list(Model model, @RequestParam(required = false, defaultValue = "0", value = "page") int page){
         Page<Posts> listPage = postsService.list(page);
@@ -41,17 +38,23 @@ public class PostsApiController {
         model.addAttribute("posts", listPage.getContent());
         model.addAttribute("totalPage", totalPage);
 
-        return Integer.toString(totalPage);  // 프론트 페이지랑 연결이 안돼서 totalPage 변경되는걸로 로짇 확인함
+        //ModelAndView mav = new ModelAndView("list");
+
+        return "list";
     }
+    */
+    @GetMapping("/board/{category}")
+    public ModelAndView list(Model model, @RequestParam(required = false, defaultValue = "0", value = "page") int page){
+        Page<Posts> listPage = postsService.list(page);
 
-    /*
-    @GetMapping("/posts/{id}")
-    public PostsResponseDto findById(@PathVariable Long id){
+        int totalPage = listPage.getTotalPages();
 
-        return postsService.findById(id);
+        model.addAttribute("posts", listPage.getContent());
+        model.addAttribute("totalPage", totalPage);
+
+        ModelAndView mav = new ModelAndView("list");
+        return mav;
     }
-
-     */
 
     /*게시글 삭제*/
     @DeleteMapping("/api/v1/posts/{id}")
