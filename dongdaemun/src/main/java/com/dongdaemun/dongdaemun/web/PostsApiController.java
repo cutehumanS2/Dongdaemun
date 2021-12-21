23eb724d9efa521035e3dd74203e7425e8c6e712
cutehumanS2 <60508828+cutehumanS2@users.noreply.github.com>
2021-12-22 01:16:53 +0900

    URL - RequestParam 방식으로 변경

--- dongdaemun/src/main/java/com/dongdaemun/dongdaemun/web/PostsApiController.java
+++ dongdaemun/src/main/java/com/dongdaemun/dongdaemun/web/PostsApiController.java
@@ -21,44 +21,43 @@ public class PostsApiController {
     /* 게시글 수정 */
     // 조회와 같은 기능. 단, 댓글은 안가져오고 게시글만 가져옴
     // 프론트가 제이슨 데이터를 받아서 뿌려줘야함
-    /*
-    @GetMapping("/update/{category}/{id}")
-    public ResponseEntity<PostsResponseDto> update(@PathVariable String category, @PathVariable Long id){
+    @GetMapping("/update")
+    public ResponseEntity<PostsResponseDto> update(@RequestParam("category") String category, @RequestParam("id") Long id){
         return ResponseEntity.ok()
                 .body(postsService.findById(id));
     }
 
-     */
+
 
     /* 게시글 수정 */
     // 게시글 저장(savepost)와 같은 기능. 변경된 내용을 프론트가 받아서 전송하면
     // 그 내용을 db에 저장함
-    @PutMapping("/update/{category}/{id}")
-    public ResponseEntity<?> update(@PathVariable String category, @PathVariable Long id, @RequestBody PostsUpdateRequestDto updateRequestDto){
+    @PutMapping("/update")
+    public ResponseEntity<?> update(@RequestParam("category") String category, @RequestParam("id") Long id, @RequestBody PostsUpdateRequestDto updateRequestDto){
         return ResponseEntity.ok()
                 .body(postsService.update(id, updateRequestDto));
     }
 
     /* 게시글 조회 */
     // 댓글 전체 조회
-    @GetMapping("/view/{category}/{id}/scroll")
-    public ResponseEntity<PostsAndCommentsResponseDto> postview(@PathVariable String category, @PathVariable Long id){
+    @GetMapping("/view/scroll")
+    public ResponseEntity<PostsAndCommentsResponseDto> postview(@RequestParam("category") String category, @RequestParam("id") Long id){
         return ResponseEntity.ok()
                 .body(postsService.findPostAndCommentsById(id));
     }
 
     /* 게시글 조회 */
     // 댓글 페이징 조회
-    @GetMapping("/view/{category}/{id}")
-    public ResponseEntity<PostsAndCommentsPageResponseDto> postview(@PathVariable String category, @PathVariable Long id
+    @GetMapping("/view")
+    public ResponseEntity<PostsAndCommentsPageResponseDto> postview(@RequestParam("category") String category, @RequestParam("id") Long id
     , @RequestParam(required = false, defaultValue = "0", value = "commentPage") int page){
         return ResponseEntity.ok()
                 .body(postsService.findPostsAndCommentsWithPageById(id, page));
     }
 
     /* 게시판 목록 조회 */
-    @GetMapping("/list/{category}")
-    public ResponseEntity<?> list(Model model, @PathVariable String category, @RequestParam(required = false, defaultValue = "0", value = "page") int page){
+    @GetMapping("/list")
+    public ResponseEntity<?> list(Model model, @RequestParam("category") String category, @RequestParam(required = false, defaultValue = "0", value = "page") int page){
         Page<?> listPage = null;
         int totalPage;
         listPage = postsService.list(category, page);
@@ -72,8 +71,8 @@ public class PostsApiController {
     }
 
     /* 게시글 삭제 */
-    @DeleteMapping("/delete/{category}/{id}")
-    public ResponseEntity<?> delete(@PathVariable Long id, @PathVariable String category){
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> delete(@RequestParam("id") Long id, @RequestParam("category") String category){
         postsService.delete(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
