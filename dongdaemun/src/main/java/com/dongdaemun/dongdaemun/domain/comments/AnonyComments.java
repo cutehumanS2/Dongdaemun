cba99a5c9ab38c9f83a3cc4993f8a1fc4bad9fe4
cutehumanS2 <60508828+cutehumanS2@users.noreply.github.com>
2021-12-21 13:40:29 +0900

    댓글: AnnonyComments Class & Service 구현

--- dongdaemun/src/main/java/com/dongdaemun/dongdaemun/domain/comments/AnonyComments.java
+++ dongdaemun/src/main/java/com/dongdaemun/dongdaemun/domain/comments/AnonyComments.java
@@ -1,4 +1,51 @@
 package com.dongdaemun.dongdaemun.domain.comments;
 
-public class AnonyComments {
+import com.dongdaemun.dongdaemun.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class AnonyComments extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long cmtId;
+
+    private Long cmtPid; //
+
+    //@ManyToOne
+    //@JoinColumn(name ="noticeFK", insertable=false, updatable=false)
+    //private NoticePosts pid;
+
+    private Long pid;
+    private String uid;
+
+    @Column(nullable = false)
+    private int cmt_index;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    String cmt_content;
+
+    @Column(nullable = false)
+    boolean cmt_anony;
+
+
+    @Builder
+    public AnonyComments(Long cmt_pid, String uid, Long pid, int cmt_index, String cmt_content, boolean anony){
+        this.cmtPid = cmt_pid;
+        this.uid = uid;
+        this.pid = pid;
+        this.cmt_index = cmt_index;
+        this.cmt_content = cmt_content;
+        this.cmt_anony = true;
+    }
+
+    public AnonyComments update(String cmt_content){
+        this.cmt_content = cmt_content;
+        return this;
+    }
 }
--- dongdaemun/src/main/java/com/dongdaemun/dongdaemun/service/comments/AnonyCommentsService.java
+++ dongdaemun/src/main/java/com/dongdaemun/dongdaemun/service/comments/AnonyCommentsService.java
@@ -1,4 +1,58 @@
 package com.dongdaemun.dongdaemun.service.comments;
 
+import com.dongdaemun.dongdaemun.domain.comments.AnonyComments;
+import com.dongdaemun.dongdaemun.domain.comments.AnonyCommentsRepository;
+import com.dongdaemun.dongdaemun.domain.comments.NoticeComments;
+import com.dongdaemun.dongdaemun.domain.comments.NoticeCommentsRepository;
+import com.dongdaemun.dongdaemun.web.dto.comments.CommentsListResponseDto;
+import com.dongdaemun.dongdaemun.web.dto.comments.CommentsResponseDto;
+import com.dongdaemun.dongdaemun.web.dto.comments.CommentsSaveRequestDto;
+import com.dongdaemun.dongdaemun.web.dto.comments.CommentsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
 public class AnonyCommentsService {
+    private final AnonyCommentsRepository anonyCommentsRepository;
+
+    @Transactional
+    public AnonyComments save(CommentsSaveRequestDto requestDto){ //Long -> Posts로 바꾸면 되나.. getId() 지우고?
+        return anonyCommentsRepository.save(requestDto.toEntityAnony());
+    }
+
+    @Transactional
+    public AnonyComments update(Long id, CommentsUpdateRequestDto requestDto){
+        AnonyComments comments = anonyCommentsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다. id=" + id));
+
+        return comments.update(requestDto.getCmt_content());
+    }
+
+    public CommentsResponseDto findById (Long id){
+        AnonyComments comments = anonyCommentsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다. id="+id));
+        return new CommentsResponseDto(comments);
+    }
+
+
+
+    // 전체 조회
+    @Transactional
+    public List<CommentsListResponseDto> findAllDesc(Long pid){
+        return anonyCommentsRepository.findAllByPid(pid).stream()
+                .map(CommentsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        AnonyComments comments = anonyCommentsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 댓글이 없습니다. id=" + id));
+        anonyCommentsRepository.delete(comments);
+    }
 }
