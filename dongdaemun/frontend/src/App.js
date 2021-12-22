0132646bf25524a5554c0c38217c9bcbd76f6247
heleneunji <heleneunji@gmail.com>
2021-12-23 04:32:07 +0900

    add mainTable, mypageTable

--- dongdaemun/frontend/src/App.js
+++ dongdaemun/frontend/src/App.js
@@ -1,31 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import PostMain from './page/post/PostMain';
-import { Carousel, Modal, Navbar, Container, Row, Col, Button,Alert,Breadcrumb, BreadcrumbItem, Card, Form, Nav } from 'react-bootstrap';
-import Pagination from './page/post/Pagination';
-import mypageComment from './page/post/mypageComment';
-import mypagePost from './page/post/mypagePost';
-import { BrowserRouter as BrowserRouter, Route, Routes, Link } from 'react-router-dom';
-import Notice from './page/post/Notice';
+import MainTable from './component/table/MainTable';
+import PageTable from './component/table/PageTable';
+import MypageComment from './component/table/MypageComment';
+import MypagePost from './component/table/MypagePost';
 
 
 function App () {
 return (
   <div className="App">
-    <BrowserRouter>
-      <div className='Menu-wrapper'>
-            <Link to='/notice'><li>Notice</li></Link>
-            <Link to='/mypagecomment'><li>mypageComment</li></Link>
-            <Link to='/mypagepost'><li>mypagePost</li></Link>
-        </div>
-        <div className='Contents-wrapper'>
-      <Routes>
-        <Route path='/notice' element={<Notice/>}></Route>
-        <Route path='/mypagecomment' element={<mypageComment/>}></Route>
-        <Route path='/mypagepost' element={<mypagePost/>}></Route>
-      </Routes>
-      </div>
-    </BrowserRouter>
+    <PageTable />
+    <MypageComment />
+    <MypagePost />
   </div>
   )
 }
