236ccfe2f4dd4f8085082d0509647e5180b02e15
cutehumanS2 <60508828+cutehumanS2@users.noreply.github.com>
2021-12-23 01:20:23 +0900

    마이페이지 구현 & 컨트롤러 반환 타입 수정

--- dongdaemun/src/main/java/com/dongdaemun/dongdaemun/web/IndexController.java
+++ dongdaemun/src/main/java/com/dongdaemun/dongdaemun/web/IndexController.java
@@ -3,19 +3,22 @@ package com.dongdaemun.dongdaemun.web;
 import com.dongdaemun.dongdaemun.config.auth.LoginUser;
 import com.dongdaemun.dongdaemun.config.auth.dto.SessionUser;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class IndexController {
 
     private final HttpSession httpSession;
 
-    /*
+
 
     @GetMapping("/")
     public ResponseEntity<?> index(Model model, @LoginUser SessionUser user){
@@ -25,8 +28,8 @@ public class IndexController {
         }
         return new ResponseEntity<>(model, HttpStatus.OK);
     }
-     */
 
+ /*
     @GetMapping("/")
     public String index(Model model, @LoginUser SessionUser user) {
         //model.addAttribute("posts", postsService.findAllDesc());
@@ -34,7 +37,7 @@ public class IndexController {
             model.addAttribute("userName", user.getName());
         }
         return "index";
-    }
+    }*/
 
 
 
