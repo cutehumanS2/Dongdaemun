f811c8dbad2c45de2787e42ac2a7832d5e747f3a
Hyun-ko <86053064+HyunkoDev@users.noreply.github.com>
2021-12-23 22:23:14 +0900

    edit app.js

--- dongdaemun/frontend/src/App.js
+++ dongdaemun/frontend/src/App.js
@@ -1,15 +1,10 @@
-//App.js
 import React from 'react';
-import NavBar from './components/NavBar/NavBar';
-import ReadPost from './components/Post/ReadPost';
 
 function App () {
 
 return (
   <div className="App">
-    <NavBar/>
-    <ReadPost/>
-
+    
   </div>
 )}
 export default App;
