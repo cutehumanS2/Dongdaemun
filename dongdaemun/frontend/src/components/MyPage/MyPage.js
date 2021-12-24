30cf340f5280147b4c5ff671f4ed79b4f4872df0
seohyun319 <tjguselly0319@gmail.com>
2021-12-24 14:23:18 +0900

    FEAT: get DB of MyPage using axios

--- dongdaemun/frontend/src/components/MyPage/MyPage.js
+++ dongdaemun/frontend/src/components/MyPage/MyPage.js
@@ -1,39 +1,49 @@
 import React from 'react'
 import './MyPage.css';
+import axios from "axios"
+import { useState, useEffect } from 'react';
 
 function MyPage() {
-  const clubName = "GDSC sookmyung"
-  const user = "김서현"
-  const posts = [
-    {
-      id:1,
-      title: "첫 번째 글",
-      date:"21.12.18"
-    }, {
-      id:2,
-      title: "두 번째 글",
-      date:"21.12.18"
-    }, {
-      id:3,
-      title: "세 번째 글",
-      date:"21.12.18"
-    }
-  ];
+  const [name, setName] = useState();
+  const [club, setClub] = useState();
+  const [posts, setPosts] = useState();
+  const [comments, setComments] = useState();
+    
+  const getData = async () => {
+    const response = await axios.get('/mypage');
+    setName(response.data.myProfileResponseDto.name);
+    setClub(response.data.myProfileResponseDto.club);
+    setPosts(response.data.postsPage.content);
+    setComments(response.data.commentsPage.content);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  
   const renderPosts = posts && posts.map(post => {
-      return (
-        <div className='post' key={post.id}>
-          <div>{post.title}</div>
-          <div>{post.date}</div>
-        </div>
-      )
-    })
+    return (
+      <div className='post' key={post.id}>
+        <div>{post.title}</div>
+        <div>{post.createDate2}</div>
+      </div>
+    )
+  })
+  const renderComments = comments && comments.map(comment => {
+    return (
+      <div className='post' key={comment.id}>
+        <div>{comment.title}</div>
+        <div>{comment.createDate2}</div>
+      </div>
+    )
+  })
 
   return (
     <div className='MyPage'>
       <div className='userInfo'>
         <div className='clubUser'>
-          <div className='clubName'>{clubName}</div>
-          <div className='user'>{user}</div>        
+          <div className='clubName'>{club}</div>
+          <div className='user'>{name}</div>  
         </div>
         <div>
           <button className='logout btn'>로그아웃</button>            
@@ -50,7 +60,7 @@ function MyPage() {
         <div>내가 쓴 댓글</div>
         <div>더보기</div>
       </div>
-      <div className='postContainer'>{renderPosts}</div>
+      <div className='postContainer'>{renderComments}</div>
     </div>
   )
 }
