e8989ad01b997115eba89f30f58ba62d7cbd194f
nami-koko <namitech3@gmail.com>
2021-12-25 23:10:41 +0900

    FEAT: get DB of ReadPost

--- dongdaemun/frontend/src/App.js
+++ dongdaemun/frontend/src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
-import ReadPost from './components/Post/ReadPost';
-import NavBar from './components/NavBar/NavBar';
 
 function App () {
 
 return (
-  <div className="App">
-    <NavBar/>
-    <ReadPost/>
-  </div>
+<div className="App">
+
+</div>
 )}
 export default App;
--- dongdaemun/frontend/src/components/Post/ReadPost.js
+++ dongdaemun/frontend/src/components/Post/ReadPost.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./ReadPost.css";
 import NavBar from "../NavBar/NavBar";
 import { Button } from "react-bootstrap";
@@ -28,32 +28,50 @@ function ReadPost() {
   //       console.error(error)
   //     })
   // }
-  const [posts, setPosts] = useState()
+  const [posts, setPosts] = useState({})
 
   const getData = async () => {
     const headers = {
       "Content-Type": "application/json"
     }
-      const response = await axios.get('http://localhost:8080/view?category=notice&id=2&page=0', {headers});
-      console.log(response.data);
-    };
-    console.log("출력!!!!"+posts);
-  
-    useEffect(() => {
+    const response = await axios.get('http://ec2-15-165-95-188.ap-northeast-2.compute.amazonaws.com:8080/view?category=notice&id=2&page=0', { headers });
+    console.log(response.data);
+    // console.log('post',[[posts, ...response.data.posts]])
+    // setPosts([posts, ...response.data.posts]);
+
+
+    setPosts({ posts, ...response.data.posts });
+  };
+  console.log("출력!!!!" + posts);
+
+  useEffect(() => {
     getData();
-    }, []); 
-  
+  }, []);
 
   return (
-    
+
     <div className="App">
-      <div className="post-title">제목</div>
+      <div className="post-wrapper">
+        {/* <div className="post-title">제목</div>
       <div className="post-writer">글 작성자</div>
       <div className="post-date">글 작성일</div>
       <div className="post-text">
 
           게시글 내용
+      </div> */}
+        {!!posts && (
+          <div className="post-section" >
+            <div className="post-title">{posts.title}</div>
+            <div className="post-writer">{posts.uid}</div>
+            <div className="post-date">{posts.createDate2}</div>
+            <div className="post-text">
+            {posts.content}
+            </div>
+          </div>
+        )}
       </div>
+
+
       <div className="post-btn-row">
         <Button className="rewrite-btn" size="sm" variant="light">
           수정하기
@@ -61,9 +79,9 @@ function ReadPost() {
         <Button className="delete-btn" size="sm" variant="light">
           삭제하기
         </Button>
-        <Recommend/>
+        <Recommend />
       </div>
-    </div>
+    </div >
   );
 }
 
