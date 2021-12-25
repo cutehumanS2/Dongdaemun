c43db27fd3ee077dcc0bc7109482662ef6ee05aa
Hyun-ko <86053064+HyunkoDev@users.noreply.github.com>
2021-12-25 14:43:08 +0900

    fix: save the temp files

--- dongdaemun/frontend/src/App.js
+++ dongdaemun/frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import { Navbar } from 'react-bootstrap';
+import ReadPost from './components/Post/ReadPost';
+import NavBar from './components/NavBar/NavBar';
 function App () {
 
 return (
   <div className="App">
-    
+    <NavBar/>
+    <ReadPost/>
   </div>
 )}
 export default App;
--- dongdaemun/frontend/src/components/Post/ReadPost.js
+++ dongdaemun/frontend/src/components/Post/ReadPost.js
@@ -1,24 +1,60 @@
 import React from "react";
+import { useState, useEffect} from "react";
 import "./ReadPost.css";
 import NavBar from "../NavBar/NavBar";
 import { Button } from "react-bootstrap";
 import Recommend from "./Comments";
+import axios from "axios";
 
 
 
 function ReadPost() {
+  const [postText, setPostText] = useState([]);
 
+  // const baseUrl = "http://localhost:8080"
+
+
+  // useEffect(() => {
+  //   getPost();
+  // }, []);
+
+  // async function getPost() {
+  //   await axios
+  //     .get("http://localhost:8080/view")
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       setPostText(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error)
+  //     })
+  // }
+  const [posts, setPosts] = useState()
+
+  const getData = async () => {
+    const headers = {
+      "Content-Type": "application/json"
+    }
+      const response = await axios.get('/view?category=notice&id=2&page=0', {headers});
+      console.log(response.data);
+    };
+    console.log("출력!!!!"+posts);
   
+    useEffect(() => {
+    getData();
+    }, []);
+
+
+
+
 
   return (
-    
+ 
     <div className="App">
       <div className="post-title">제목</div>
       <div className="post-writer">글 작성자</div>
       <div className="post-date">글 작성일</div>
       <div className="post-text">
-
-          게시글 내용
       </div>
       <div className="post-btn-row">
         <Button className="rewrite-btn" size="sm" variant="light">
